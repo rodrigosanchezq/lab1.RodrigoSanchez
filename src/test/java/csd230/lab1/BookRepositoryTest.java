68a8e886e4d20aa97e0a801ff366b8c6247c3e74
Rodrigo Sanchez <23015753@saultcollege.ca>
2025-04-14 00:47:33 -0400

    initial commit

--- src/test/java/csd230/lab1/BookRepositoryTest.java
+++ src/test/java/csd230/lab1/BookRepositoryTest.java
@@ -2,21 +2,9 @@ package csd230.lab1;
 
 import org.junit.jupiter.api.Test;
 
-class BookRepositoryTest {
+
 
     @Test
-    void findByIsbn() {
-    }
 
-    @Test
-    void findById() {
-    }
-
-    @Test
-    void findByPriceRange() {
-    }
-
-    @Test
-    void findByAuthorLike() {
     }
 }
